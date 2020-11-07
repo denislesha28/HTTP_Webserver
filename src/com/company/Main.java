708477e555bebe9c79309f9621ad1f5acd7ed32f
Denis Lesha <denislesha8@gmail.com>
2020-11-07 19:29:11 +0100

    V1 Working Connection

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int portNumber = 1111;
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            if(clientSocket!=null) {
+                System.out.println("Connected");
+            }
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
