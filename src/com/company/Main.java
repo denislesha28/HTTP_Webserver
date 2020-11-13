73a09648cf6ae77e20ffb8bd021fc0fa989d39d2
Denis Lesha <denislesha8@gmail.com>
2020-11-13 15:57:16 +0100

    Switched Implementation to saving in ArrayLists

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,15 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         int portNumber = 1111;
-
+        List<String> messages=new ArrayList<>();
+        List<String> headers=new ArrayList<>();
         try {
             ServerSocket serverSocket = new ServerSocket(portNumber);
             Socket clientSocket = serverSocket.accept();
@@ -21,9 +24,10 @@ public class Main {
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             RequestContext handler=new RequestContext();
-            handler.readHeader(in);
-            handler.readPayload(in);
-            handler.savePayload();
+            int responseId=handler.saveHTTPHeader(headers,in);
+            int responseIdMessage=handler.savePayload(messages,in);
+            System.out.println(messages);
+            System.out.println(headers);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/com/company/RequestContext.java
+++ src/com/company/RequestContext.java
@@ -4,8 +4,6 @@ import com.sun.beans.decoder.StringElementHandler;
 import jdk.nashorn.internal.parser.JSONParser;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,18 +11,24 @@ import java.util.List;
 public class RequestContext {
     private StringBuilder payload;
     private String headerInfo;
-    public List<String> messages=new ArrayList<>();
-    private String filepath="messages";
 
     public void readHeader(BufferedReader in) throws IOException {
-        while((headerInfo=in.readLine()).length() != 0){
-            System.out.println(headerInfo);
+        String line;
+        while((line=in.readLine()).length() != 0){
+            System.out.println(line);
+            if(line.compareTo("null")==0){
+                continue;
+            }
+            headerInfo=headerInfo+line+"\n";
         }
+        headerInfo=headerInfo.replace("null","");
     }
 
-    public void saveHTTPHeader(){
-
-
+    public int saveHTTPHeader(List<String> list,BufferedReader in) throws IOException {
+        readHeader(in);
+        list.add(headerInfo);
+        System.out.println(headerInfo);
+        return list.size();
     }
 
     public String readHTTPVerb(BufferedReader in) throws IOException {
@@ -41,40 +45,11 @@ public class RequestContext {
         // System.out.println("Payload data is: "+payload.toString());
     }
 
-    public void savePayload() throws IOException {
-        // find all occurrences forward
-       /* for (int i = -1; (i = payload.indexOf("userId", i + 1)) != -1; i++) {
-            System.out.println(i);
-        }*/
-        File file=new File(filepath);
-        if(file.mkdirs()){
-            System.out.println("Successful Folder Creation");
-        }
-        else if(file.isDirectory()){
-            System.out.println("Folder already exists");
-        }
-        else{
-            System.out.println("Unsuccessful Folder Creation");
-            return;
-        }
-        filepath=filepath+"\\";
-        int i=11;
-        while(payload.charAt(i)!=','){
-            filepath=filepath+payload.charAt(i);
-            i++;
-        }
-        file=new File(filepath);
-        file.createNewFile();
-        FileWriter writer=new FileWriter(file);
-        writer.write(String.valueOf(payload));
-        writer.flush();
-        writer.close();
+    public int savePayload(List<String> list,BufferedReader in) throws IOException {
+        readPayload(in);
+        list.add(payload.toString());
+        payload=null; // empty payload
+        return list.size();
     }
 
-
-   /* public void savePayload() {
-        messages.set(2,"ckyaaddawd");
-        messages.set(payload.charAt(11),payload.toString());
-    }
-*/
 }
